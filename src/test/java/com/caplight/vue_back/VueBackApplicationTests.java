37325e500a57c326b83d22aa2820c2dd4f0ca2b9
378603374 <378603374@qq.com>
2019-11-30 13:44:07 +0800

    Initial commit

---
+++ src/test/java/com/caplight/vue_back/VueBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caplight.vue_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
